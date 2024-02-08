b76bee2690cd7ebce27e7288bbdd1535c2cd2603
CAMILA ROCHA DE ANDRADE <camila.randrade@senacsp.edu.br>
2024-02-07 21:00:20 -0300

    atualização atividade

---
+++ script.js
@@ -0,0 +1,44 @@
+// Classe Quarto:
+// Representa um quarto de hotel com atributos como número, tipo, preço da diária e estado de reserva.
+
+class Quarto {
+    constructor (numero, tipo, preco, reserva) {
+    this.numero= numero;
+    this.tipo= tipo;
+    this.preco= preco;
+    this.reserva= false; 
+
+}
+
+numero () {
+
+}
+
+tipo () {
+
+}
+
+preco () {     
+
+}
+
+reserva () {
+
+}
+
+verificarDisponibilidade () {
+
+}
+
+}
+
+// Classe Hospede:
+// Representa um hóspede do hotel com atributos como nome e e-mail.
+
+class Hospede {
+    constructor (nome, email) {
+        this.nome= nome;
+        this.email= email;
+         
+    }
+}
